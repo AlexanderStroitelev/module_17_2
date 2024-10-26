052afc36eabef5f8a25c339c067dd864f70a8537
Aleksandr Stroitelev <36483253+AlexanderStroitelev@users.noreply.github.com>
2024-10-26 23:44:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,25 +13,3 @@ async def wellcome():
 
 app.include_router(task.router)
 app.include_router(user.router)
-
-
-Base.metadata.create_all(bind=engine)
-print()
-
-# информация о таблицах и их колонках
-for table_name, table in Base.metadata.tables.items():
-    print(f"Таблица: {table_name}")
-
-    # их свойства
-    print("Столбцы:")
-    for column in table.columns:
-        print(f"{column.name} - Тип: {column.type}")
-        if column.primary_key:
-            print(f"      Это первичный ключ")
-
-    # связи (ForeignKey)
-    print("Связи:")
-    for fk in table.foreign_keys:
-        print(f"      {fk.column} -> {fk.target_fullname}")
-
-    print()
