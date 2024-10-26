df6145805479f9ed1ff1b558d97314a3af4d76c0
Aleksandr Stroitelev <36483253+AlexanderStroitelev@users.noreply.github.com>
2024-10-26 23:41:22 +0300

    Update task.py

--- task.py
+++ task.py
@@ -34,7 +34,6 @@ async def delete_task():
 
 class Task(Base):
     __tablename__ = "tasks"
-
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
@@ -42,6 +41,8 @@ class Task(Base):
     completed = Column(Boolean, default=False)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False, index=True)
     slug = Column(String, unique=True, index=True)
-
-    # Связь с моделью User
     user = relationship("User", back_populates="tasks")
+
+from sqlalchemy.schema import CreateTable
+
+print(CreateTable(Task.__table__))
